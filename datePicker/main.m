23481f13fbfd8443fc92943bab724bc86406e64b
Rob Mayoff <git@rob.dqd.com>
2015-10-22 23:43:08 -0500

    Initial Commit

---
+++ datePicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  datePicker
+//
+//  Created by Rob Mayoff on 10/22/15.
+//  Copyright © 2015 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
